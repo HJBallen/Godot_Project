2585956569b212eaa33343292a5ae8d5f28d30f8
StormBlessed27 <hjballent@gmail.com>
2024-04-30 07:59:59 -0500

    Se agrego el script para el jefe

---
+++ scripts/jefe.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+@onready var player = get_node("/root/Stage/Player")
+
+var health := 3
+var hurt : bool
+var dead : bool
+
+func _physics_process(delta):
+	
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 160
+	if not hurt:
+		move_and_slide()
+
+
+
+
+func take_damage():
+	health -= 1
+	hurt = true
+	if health == 0:
+		dead = true
+
+#func on_animations_finished(anim_name):
+	#if anim_name == "hurt":
+		#hurt = false
+	#if anim_name == "die":
+		#queue_free()
