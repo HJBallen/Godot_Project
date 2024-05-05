22152b05545e5e448f50a502897a112eae566a8c
StormBlessed27 <hjballent@gmail.com>
2024-05-05 12:37:38 -0500

    Terminado el proyecto

--- scenes/pause_scene.gd
+++ scenes/pause_scene.gd
@@ -3,11 +3,13 @@ extends Control
 signal resume
 signal exit
 
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		resume.emit()
+
 func _on_continue_btn_pressed():
-	print("resumir")
 	resume.emit()
 
 
 func _on_exit_btn_pressed():
-	print("salir")
 	exit.emit()
